875988324b2f5d85ce0c1cafa7d14bf39675aa31
limumu008 <limumu008@gmail.com>
2019-09-18 01:40:56 -0400

    V1.2

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,8 +2,9 @@
 Application for converting .csv file to .json file
 """
 
-import csv, json, os, sys
+import csv, json, sys
 import argparse
+
 
 def csv_to_json(csvFile, jsonFile):
     # Read .csv file and store it into dictionary
@@ -23,24 +24,15 @@ def csv_to_json(csvFile, jsonFile):
         print("CSV file is complete converted!")
     except Exception as e:
         print("Error occured:", e)
-# Testing
-# csvFilePath = "pipeline_input_user_export.csv"
-# jsonFilePath = "D:\dataScience\Corteva\pipeline_input_user_export.json"
-#
-# rc = read_csv(csvFilePath)
-# wj = write_json(rc, jsonFilePath)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Convert a .csv file to a .json file")
-    parser.add_argument('-in', '--input', help="CSV input file", type=str)
-    parser.add_argument('-out', '--output', help="JSON output file",  type=str)
-    #parser.add_argument()
-    #parser.set_defaults(func=csv_to_json)
+    parser.add_argument('-in', '--csvFile', metavar='', help="CSV input file", type=str, required=True)
+    parser.add_argument('-out', '--jsonFile', metavar='', help="JSON output file",  type=str, required=True)
     args = parser.parse_args()
-    csv_to_json(args.input, args.output)
-    # csvFilePath = input("Please enter .csv file: ")
-    # jsonFilePath = os.path.splitext(csvFilePath)[0] + ".json"
-    # dt = read_csv(csvFilePath)
-    # write_json(dt, jsonFilePath)
+    csv_to_json(args.csvFile, args.jsonFile)
+
 
 if __name__ == '__main__':
     main()
