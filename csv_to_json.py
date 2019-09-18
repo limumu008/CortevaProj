d8ec1a36454e0e41f58cb92ae0336569a9423ca4
limumu008 <limumu008@gmail.com>
2019-09-18 01:13:39 -0400

    V1.1

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,38 +2,45 @@
 Application for converting .csv file to .json file
 """
 
-import csv, json, os
+import csv, json, os, sys
+import argparse
 
-
-def read_csv(file):
+def csv_to_json(csvFile, jsonFile):
     # Read .csv file and store it into dictionary
     data = {}
-
-    with open(file, "r") as csvFile:
-        csvReader = csv.DictReader(csvFile)
-        for csvRow in csvReader:
-            _id = csvRow["id"]
-            data[_id] = csvRow
-    return data
-    # try:
-    #     if csvFile
-    # csvFilePath = "pipeline_input_user_export.csv"
-    # jsonFilePath = "pipeline_input_user_export.json"
-
-
-def write_json(data, file):
+    with open(csvFile, "r") as c:
+        csvReader = csv.DictReader(c)
+        try:
+            for csvRow in csvReader:
+                _id = csvRow["id"]
+                data[_id] = csvRow
+        except csv.Error as e:
+            sys.exit('file {}, line {}: {}'.format(csvFile, csvReader.line_num, e))
     #Write data into a .json file
-    with open(file, "w") as jsonFile:
-        jsonFile.write(json.dumps(data))
-    print("JSON file is complete converted!")
+    try:
+        with open(jsonFile, "w") as j:
+            j.write(json.dumps(data, indent="\t"))
+        print("CSV file is complete converted!")
+    except Exception as e:
+        print("Error occured:", e)
 # Testing
 # csvFilePath = "pipeline_input_user_export.csv"
 # jsonFilePath = "D:\dataScience\Corteva\pipeline_input_user_export.json"
 #
 # rc = read_csv(csvFilePath)
 # wj = write_json(rc, jsonFilePath)
+def main():
+    parser = argparse.ArgumentParser(description="Convert a .csv file to a .json file")
+    parser.add_argument('-in', '--input', help="CSV input file", type=str)
+    parser.add_argument('-out', '--output', help="JSON output file",  type=str)
+    #parser.add_argument()
+    #parser.set_defaults(func=csv_to_json)
+    args = parser.parse_args()
+    csv_to_json(args.input, args.output)
+    # csvFilePath = input("Please enter .csv file: ")
+    # jsonFilePath = os.path.splitext(csvFilePath)[0] + ".json"
+    # dt = read_csv(csvFilePath)
+    # write_json(dt, jsonFilePath)
 
 if __name__ == '__main__':
-    csvFilePath = input("Please enter .csv file: ")
-    jsonFilePath = os.path.splitext(csvFilePath)[0]+".json"
-    write_json(read_csv(csvFilePath), jsonFilePath)
+    main()
