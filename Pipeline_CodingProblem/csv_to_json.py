c3bb24418a338b1edafd14f39376f0afa513eaca
limumu008 <limumu008@gmail.com>
2019-09-18 12:09:36 -0400

    V1.3

--- Pipeline_CodingProblem/csv_to_json.py
+++ Pipeline_CodingProblem/csv_to_json.py
@@ -1,13 +1,23 @@
-"""
-Application for converting .csv file to .json file
-"""
-
-import csv, json, sys
+import csv, json, os
 import argparse
+import logging
+
+log_format = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename = "pipeline.log", level = logging.DEBUG, format = log_format, filemode = 'w')
+logger = logging.getLogger()
 
 def csv_to_json(csvFile, jsonFile):
-    # Read .csv file and store it into dictionary
+    '''
+    Convert .csv file to .json file
+    :param csvFile: .csv file
+    :param jsonFile: .json file
+    :return: None
+    '''
+
+    # Create data as a dictionary for storing the data from csv file
     data = {}
+    # Read a csv file
+    logger.debug('# Open csv file')
     with open(csvFile, "r") as c:
         csvReader = csv.DictReader(c)
         try:
@@ -15,21 +25,38 @@ def csv_to_json(csvFile, jsonFile):
                 _id = csvRow["id"]
                 data[_id] = csvRow
         except csv.Error as e:
-            sys.exit('file {}, line {}: {}'.format(csvFile, csvReader.line_num, e))
+            logging.exception('file {}, line {}: {}'.format(csvFile, csvReader.line_num, e))
     #Write data into a .json file
+    logger.debug('# Write json file')
     try:
         with open(jsonFile, "w") as j:
             j.write(json.dumps(data, indent="\t"))
-        print("CSV file is complete converted!")
+        logger.debug("# CSV file is complete converted to JSON file!")
     except Exception as e:
-        print("Error occured:", e)
+        logging.exception(e)
 
 def main():
+    '''
+    CLI program defines .csv file as an input and .json file as an output argument.
+    It generates the help, useage messages and errors with logging.
+    '''
     parser = argparse.ArgumentParser(description="Convert a .csv file to a .json file")
     parser.add_argument('-in', '--csvFile', metavar='', help="CSV input file", type=str, required=True)
     parser.add_argument('-out', '--jsonFile', metavar='', help="JSON output file",  type=str, required=True)
     args = parser.parse_args()
-    csv_to_json(args.csvFile, args.jsonFile)
+    logger.info("csv_to_json({0}, {1})".format(args.csvFile, args.jsonFile))
+    try:
+        if os.path.splitext(args.csvFile)[1] != '.csv':
+            logger.error('# Input file has to be a csv file! e,g test.csv ')
+            raise FileNotFoundError
+        if os.path.splitext(args.jsonFile)[1] != '.json':
+            logger.error('# Output file has to be a json file! e,g test.json')
+            raise FileExistsError
+        else:
+            csv_to_json(args.csvFile, args.jsonFile)
+    except (FileExistsError, FileNotFoundError) as e:
+        logging.exception(e)
+        raise
 
 if __name__ == '__main__':
     main()
